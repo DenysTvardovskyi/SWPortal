e9e84675109df9ced977951bce95fe2d01796663
DenisTvardovskiy <40178797+DenisTvardovskiy@users.noreply.github.com>
2021-03-18 12:05:08 +0200

    fixed pagination & more info blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,8 @@
 let node = null;
 let full = null;
 let namePi = null
+let searchNode = null;
+let fes = [];
 let mainUrl = "http://swapi.dev/api/planets/?page=1";
 
 let gridSelector = document.getElementById("gridAjust")
@@ -28,22 +30,24 @@ gridSelector.addEventListener("change", _=>{
     }
 })
 
-function planetsAp() {
-    node = fetch(mainUrl)
+
+async function planetsAp() {
+    node = await fetch(mainUrl)
         .then(response => response.json())
         .then(result => node = result );
+    const laod = document.addEventListener("load", test())
     
 };
-function nameP(link) {
+async function nameP(link) {
     
-    full= fetch(link)
+    full=  await fetch(link)
         .then(response => response.json())
         .then(data => full = data );
     return full
 }
-function namePicker(link) {
+async function namePicker(link) {
     
-    namePi= fetch(link)
+    namePi= await fetch(link)
         .then(response => response.json())
         .then(data => namePi = data );
     return namePi
@@ -57,9 +61,10 @@ function closeInfoBlock() {
 function reply_id(clicked_id){
     document.getElementById("planetInfoBlock").style.display = 'flex'
     let planet = node.results[clicked_id]
-
+    linkToName(planet)
+    linkToFilm(planet)
     
-    let result =
+    let result = 
                 `
                 <button onclick="closeInfoBlock()">Close</button>
                 <div>
@@ -75,13 +80,12 @@ function reply_id(clicked_id){
                         <li>Surface Water: ${planet.surface_water} %</li>
                         <li>Population: ${planet.population}</li>
                         <li>Residents:                          
-                            <ul> 
-                            ${planet.residents.length ===0? "none":  linkToName(planet)}
+                            <ul id="resNames"> 
                             </ul>
                         </li>
                         <li>Films: 
-                            <ul> 
-                                ${planet.films.length ===0? "none": linkToFilm(planet) }
+                            <ul id="filmTit"> 
+                                
                             </ul>
                         </li>
                     </ul>
@@ -90,95 +94,109 @@ function reply_id(clicked_id){
 
 }
 
-const DETAILS_NAME = (link)=>{
-    namePicker(link).then((a)=>{
-        fes=""
-        fes= ""+ a.name + " - " + a.gender +" - "+a.birth_year+"; <br> " 
-
+ const  DETAILS_NAME = async (link)=>{
+    await namePicker(link).then((a)=>{
+        fes+= ""+ a.name + " - " + a.gender +" - "+a.birth_year+"; <br> " 
     })
-    return fes
+    return fes  
+    
+    
 }
 
-function linkToName(planet) {
+
+async function linkToName(planet) {
+    fes = ''
     let links = planet.residents
-    let m = ''
     if(links.length==0){
         return "none"
     }else{
         for(let i =0; i<links.length;i++){
-                m +=DETAILS_NAME(links[i])     
-
+           await DETAILS_NAME(links[i])
         }
-        return m
+        document.getElementById("resNames").innerHTML = fes
     }
+    
     
 }
 
-const DETAILS = (link)=>{
-    nameP(link).then((a)=>{
-        res =""
+const DETAILS = async (link)=>{
+    await nameP(link).then((a)=>{
         res+= ""+ a.title + " - " + a.release_date +"; <br> " 
-        
     })
     return res
 }
 
-function linkToFilm(planet) {
+async function linkToFilm(planet) {
+    res = ''
     let linksF = planet.films
-    let g = []
-    while(linksF.length >0){
-        setInterval(g.push(DETAILS(linksF[0])),3000)
-        linksF.shift()
+    if(linksF.length==0){
+        return "none"
+    }else{
+        for(let i =0; i<linksF.length;i++){
+           await DETAILS(linksF[i])
+        }
+        document.getElementById("filmTit").innerHTML = res
     }
-    return g
-
+    
+    
 }
 
+
+
+
+async function search(key) {
+    let dir = `https://swapi.dev/api/planets/?search=${key}`
+    searchNode = await fetch(dir)
+    .then(response => response.json())
+        .then(data => searchNode = data );
+    return searchNode
+}
+
+function searchBody() {
+    let ped = document.getElementById("main")
+    ped.innerHTML=''
+    for (let i = 0; i < searchNode.results.length; i++){
+        let result1 =
+        `
+        <div class="item">
+            <div class="info">
+                <div class="basic-info">
+                    <li>Name: ${searchNode.results[i].name}</li>
+                    <li>Diameter: ${searchNode.results[i].diameter}km</li>
+                    <li>Population: ${searchNode.results[i].population}</li>
+                </div>
+                <div class="more-info">
+                    <li id="${i}" onclick="reply_id(this.id)">More Info</li>
+                </div>
+            </div>
+        </div>
+        ` 
+        ped.innerHTML += result1
+    }
+}
 
 
 function test() {
     let ped = document.getElementById("main")
     ped.innerHTML=''
-    
-    
-    
-    console.log(node);
-    // for(let i= 0; i < node.results.length; i++) {
-    //     if(node.results[i].name === "Callisto") {
-    //         console.log(node.results[i]);
-    //     }
-    // }
-    let countries = document.getElementById('main');
     for (let i = 0; i < node.results.length; i++){
-        let parantDiv = document.createElement("div")
-        parantDiv.classList.add("item")
-        let info  = document.createElement("div")
-        info.classList.add("info")
-        let basicinfo = document.createElement("div")
-        basicinfo.classList.add("basic-info")
-        let basicInfoName = document.createElement("li")
-        basicInfoName.innerText = "Name: "+ node.results[i].name
-        let basicInfoDiameter = document.createElement("li")
-        basicInfoDiameter.innerText = "Diameter: "+node.results[i].diameter +"km"
-        let basicInfoPopulation = document.createElement("li")
-        basicInfoPopulation.innerText = "Population: "+node.results[i].population
-        basicinfo.appendChild(basicInfoName)
-        basicinfo.appendChild(basicInfoDiameter)
-        basicinfo.appendChild(basicInfoPopulation)
-        let moreInfo = document.createElement("div")
-        moreInfo.classList.add("more-info")
-       
-        let moreInfoMore = document.createElement("li")
-        moreInfoMore.innerText = "More Info"
-        moreInfoMore.setAttribute("id", i)
-        moreInfoMore.setAttribute("onclick", "reply_id(this.id)")
-        moreInfo.appendChild(moreInfoMore)
-        info.appendChild(basicinfo)
-        info.appendChild(moreInfo)
-        parantDiv.appendChild(info)
-        countries.appendChild(parantDiv)
+        let result1 =
+        `
+        <div class="item">
+            <div class="info">
+                <div class="basic-info">
+                    <li>Name: ${node.results[i].name}</li>
+                    <li>Diameter: ${node.results[i].diameter}km</li>
+                    <li>Population: ${node.results[i].population}</li>
+                </div>
+                <div class="more-info">
+                    <li id="${i}" onclick="reply_id(this.id)">More Info</li>
+                </div>
+            </div>
+        </div>
+        ` 
+        ped.innerHTML += result1
     }
-
 }
 
 const NextPage = ()=>{ console.log(mainUrl)
@@ -186,21 +204,6 @@ const NextPage = ()=>{ console.log(mainUrl)
         console.log("last")
     }else{
         mainUrl = node.next; planetsAp();
-    }
-
-
-
-    if(node.next === null ){
-        document.getElementById("next").style.display = 'none'
-    } else{
-        document.getElementById("next").style.display = 'inline'
-        
-        
-    }
-    if(node.previous === null ){
-        document.getElementById("prev").style.display = 'none'
-    } else{
-        document.getElementById("prev").style.display = 'inline'
     }
 }
 const PrevPage = ()=>{
@@ -210,20 +213,7 @@ const PrevPage = ()=>{
         mainUrl = node.previous; planetsAp()
     }
     
-    
-    if(node.next === null ){
-        document.getElementById("next").style.display = 'none'
-    } else{
-        document.getElementById("next").style.display = 'inline'
-        
-    }}
-    if(node.previous === null ){
-        document.getElementById("prev").style.display = 'none'
-    } else{
-        document.getElementById("prev").style.display = 'inline'
-    }
-
-
+}
     
 
 
